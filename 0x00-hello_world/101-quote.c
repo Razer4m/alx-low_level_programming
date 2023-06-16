e9c90b79ccea6fd7c2e83bca481e53313f3e74da
Razer4m <tumelomorakile@gmail.com>
2023-06-15 20:58:47 -0700

    uni

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int write = (1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
