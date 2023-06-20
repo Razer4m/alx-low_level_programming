47764de313446b20d22fe7e87a286576f3a46db8
Razer4m <tumelomorakile@gmail.com>
2023-06-20 16:18:58 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints lowercase alphabet 10 times
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i = 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
