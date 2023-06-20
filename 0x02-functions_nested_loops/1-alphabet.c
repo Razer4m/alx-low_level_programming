c50a981eaf92daeb067834588de89cb536e9632b
Razer4m <tumelomorakile@gmail.com>
2023-06-20 14:46:22 -0700

    task 0 and 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Printing lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# 0x02 C -Functions, nested loops \n loops programmes
