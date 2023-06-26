a249beb9c4dfa0b0675b0da4e834195dbe048fbc
Razer4m <tumelomorakile@gmail.com>
2023-06-26 10:28:27 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Calculates the length of a string
+ * @s: The input string
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int *count = s;
+
+	while (*count != '\0')
+	{
+		count++
+	}
+	return (count - s)
+}
