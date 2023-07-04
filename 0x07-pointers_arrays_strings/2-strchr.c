aecfd37d9e2d000576dd2f3fe9014b9202aa2725
Razer4m <tumelomorakile@gmail.com>
2023-07-04 13:06:26 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
 			return (s);
 		}
-		s++;
+		i++;
 	}
 	if (c == '\0')
 	{
