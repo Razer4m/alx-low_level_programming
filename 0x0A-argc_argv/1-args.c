4985bbb84a060aa2fc37587be2443a7dc7f1b0f7
Razer4m <tumelomorakile@gmail.com>
2023-07-07 03:18:46 -0700

    Task 0 and 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 upon successful execution.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
