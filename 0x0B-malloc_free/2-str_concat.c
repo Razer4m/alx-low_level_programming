2ccb39784164dc5d2b0fe313c6aff1c19aa1f414
Razer4m <tumelomorakile@gmail.com>
2023-07-10 21:33:48 -0700

    Task 2 to 4

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Pointer to the concatenated string, or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int k, l;
+	unsigned int i = 0, j = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	concat = malloc((i + jj + 1) * sizeof(char));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+	{
+		concat[k] = s1[k];
+	}
+	for (l = 0; l < j; l++)
+	{
+		concat[k + l] = s2[l];
+	}
+	concat[k + l] = '\0';
+	return (concat);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.c"
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of integers.
+ * @width: The width of the grid.
+ * @height: The height of the grid.
+ *
+ * Return: Pointer to the allocated grid, or NULL on failure.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			ree(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
