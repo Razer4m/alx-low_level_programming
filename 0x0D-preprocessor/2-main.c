7069534fc3c93c129f444ba7dfbf872d5b012778
Razer4m <tumelomorakile@gmail.com>
2023-07-17 05:26:07 -0700

    Task 2 C

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,7 +5,14 @@
  */
 int main(void)
 {
-	_putchar(__FILE__);
+	const char *file_name = __FILE__;
+	int i = 0;
+	
+	while (file_name[i] != '\0')
+	{
+		_putchar(file_name[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
