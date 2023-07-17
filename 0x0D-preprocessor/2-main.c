e81dbc3c05a1f97592bdbd421ad3a689d1a2b247
Razer4m <tumelomorakile@gmail.com>
2023-07-17 05:28:45 -0700

    Task 2 C

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - Prints the name of the file it was compiled from, followed by a new line.
+ * main - Prints the name of the file it was compiled from,
+ * followed by a new line.
  * Return: ALways 0 if Success
  */
 int main(void)
 {
 	const char *file_name = __FILE__;
 	int i = 0;
-	
+
 	while (file_name[i] != '\0')
 	{
 		_putchar(file_name[i]);
