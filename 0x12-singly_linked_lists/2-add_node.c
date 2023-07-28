e09f58f7b537be7e5c7a5e3fca73e07bbb562b87
Razer4m <tumelomorakile@gmail.com>
2023-07-28 21:26:37 +0200

    Task 2 Correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	unsigned int len = 0;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	unsigned int len = 0;
 
 	while (str[len])
 	{
