2a7087111974ae66c59b4ce5300c7028a94ce334
Razer4m <tumelomorakile@gmail.com>
2023-07-28 10:20:47 +0200

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
- * add_node - Adds a new node at the beginning of a lsit_t list.
+ * add_node - Adds a new node at the
+ * beginning of a lsit_t list.
  * @head: Double pointer to the head of the linked list.
  * @str: POinter to string to be duplicated and added to the new node.
  *
- * Return: the adress of a new ellement, or NULL if it failed
+ * Return: the adress of a new ellement, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -17,13 +18,19 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	char *dup_str = strdup(str);
+	if (dup_str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len =strlen(str);
+	int len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new_node->str = dup_str;
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
