1d7cacde8c6c52a24cb0c34c94dfc9800332803e
Razer4m <tumelomorakile@gmail.com>
2023-08-04 14:34:17 -0700

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	}
 
-	unsigned long int mask = 1 << index;
+	unsigned long int mask;
+
+	mask = 1 << index;
 
 	*n |= mask;
 	return (1);
