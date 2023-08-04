62f011212943809a50241033088f9a4c955c390c
Razer4m <tumelomorakile@gmail.com>
2023-08-04 16:00:30 -0700

    adv

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	char *p;
+
+	p = (char *)&number;
+
+	return (*p);
+}
