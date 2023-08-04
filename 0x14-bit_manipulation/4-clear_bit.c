d6c4f64303d6aae12c28779b2b6daae028fa564d
Razer4m <tumelomorakile@gmail.com>
2023-08-04 15:02:10 -0700

    Task 4 and 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * set_bit - Sets the value of a bit to 1 at a given index.
+ * clear_bit - Sets the value of a bit to 0 at a given index.
  * @n: Pointer to the number whose bit is to be set/
  * @index: The index of the bit to be set
  *
  * Return: 1 if success, or -1 if invalid
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-
+	
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
 	}
 	mask = 1 << index;
 
-	*n |= mask;
+	*n &= ~mask;
 	return (1);
 }
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits needed to flip to get from one
+ *             number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits needed to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x_result = n ^ m;
+	unsigned int counter = 0;
+
+	while (x_result)
+	{
+		counter += x_result & 1;
+		x_result >>= 1;
+	}
+	return (counter);
+}
