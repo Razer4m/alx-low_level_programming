e08a653617ba287906d222cfa0b7bb60a7a87949
Razer4m <tumelomorakile@gmail.com>
2023-11-17 22:30:14 +0200

    Task 0

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Amount of bytes from src
+ *
+ * Return: Dest the Pointer string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
